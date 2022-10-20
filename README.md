bc09efe4cd59adfab34ce1d28e56c895fe548649
Ismatik <mukhamejanov.ismat@gmail.com>
2022-10-20 12:06:50 +0600

    Trying to work with framework "Django"

--- README.md
+++ README.md
@@ -1,6 +1,2 @@
 #Showcase
 
-Developing Telegram Bot using guidde from this: 
-
-chrome-extension://efaidnbmnnnibpcajpcglclefindmkaj/https://riptutorial.com/Download/telegram-bot.pdf
-
--- showcase.py
+++ showcase.py
@@ -1,15 +1,2 @@
 #Showcase of the project using Django.
 #Checking with github.
-
-from concurrent.futures import thread
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-
-def index():
-    return "<h1> Welcome, my daily task bot!<h1>"
-
-if __name__ == "__main__":
-    app.run(threaded = True)
